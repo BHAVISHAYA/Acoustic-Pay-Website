5b582b9dbc3a4c14ee1c64f037c557409c8c4a8d
BHAVISHAYA <bhavishayakhandelwal@gmail.com>
2024-07-03 16:28:49 +0530

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ export const Home = () => {
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut minus reprehenderit doloribus mollitia? Fuga error perferendis hic nostrum debitis laudantium repudiandae. Repellendus praesentium placeat doloremque aliquid quam quidem sequi nesciunt odit, quasi alias? Quisquam, rerum, quo magnam ratione et esse corrupti sapiente, minima dolorum aliquam quos vitae similique. Voluptates illo ducimus hic dolorem harum iure officiis quaerat nam tenetur, cumque, officia mollitia? Suscipit blanditiis, amet nobis corporis, repudiandae perferendis voluptates nostrum dolorum numquam, ea quasi! Enim natus ab ratione ut sapiente nisi sed magnam laboriosam temporibus quaerat voluptates, nam maiores? Beatae reiciendis totam voluptatem quae expedita repellat veritatis est modi!
                         </p>
                         <h3> Get early access for you </h3>
-                        <div>
+                        <div className="input-group">
                             <input type="text" className="my-3 rounded-pill w-100 me-3 form-control-text" placeholder="Enter Your Email..." />
                             <button> get It Now </button>
                         </div>
